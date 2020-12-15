6794b67f773b592b32b9472e4dae9dacce2e33e0
yogiex <priyagung.yoggi@gmail.com>
2020-12-15 10:46:08 +0700

    fetch api

--- src/app/fetch-api.service.ts
+++ src/app/fetch-api.service.ts
@@ -3,11 +3,57 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
 import { catchError, retry } from 'rxjs/operators';
 
+// export interface User {
+//   postId: string;
+//   id: string;
+//   name: string;
+//   email: string;
+//   body: string;
+// }
+
 @Injectable({
   providedIn: 'root'
 })
 export class FetchApiService {
 
-  urlApi = 'http://www.recipepuppy.com/api'
-  constructor(private httpClient) { }
+  //urlApi = 'http://www.recipepuppy.com/api';
+  private endpoint = 'https://jsonplaceholder.typicode.com/users'
+  constructor(private httpClient: HttpClient) { }
+
+  httpHeader = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json'
+    })
+  }
+
+  // public getUsers(): Observable<User> {
+  //   return this.httpClient.get<User>(this.endpoint + '/users')
+  //   .pipe(
+  //     retry(1),
+  //     catchError(this.processError)
+  //   )
+  // }
+
+  public getData(){
+    return this.httpClient.get(this.endpoint);
+  }
+  // getSingleUser(id): Observable<User> {
+  //   return this.httpClient.get<User>(this.endpoint + '/users/' + id)
+  //   .pipe(
+  //     retry(1),
+  //     catchError(this.processError)
+  //   )
+  // }
+
+  processError(err){
+    let message = '';
+    if(err.error instanceof ErrorEvent){
+      message = err.error.message;
+    } else {
+      message = 'Error code: ${err.status}\nMessage: ${err.message}';
+    }
+    console.log(message);
+    return throwError(message);
+  }
+
 }
